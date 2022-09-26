610a8271818587aea53f170b6e6d25127180a45e
LeLop72 <91336206+LeLop72@users.noreply.github.com>
2022-09-26 21:38:35 +0300

    Update config_db.py

--- app/config_db.py
+++ app/config_db.py
@@ -1,6 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+rom sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.declarative.api import DeclarativeMeta
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///./recipe.db"
@@ -9,4 +8,4 @@ engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False,
                              class_=AsyncSession)
 local_session = async_session()
-Base: DeclarativeMeta = declarative_base()
+Base = declarative_base()
